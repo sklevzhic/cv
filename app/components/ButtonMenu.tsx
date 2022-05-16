d71116ef2afbe7d8abde94822cb407bf4b3c42d1
sklevzhic <sklevzhic@gmail.com>
2022-05-16 16:28:52 +0300

    fixed hamburger menu

--- app/components/ButtonMenu.tsx
+++ app/components/ButtonMenu.tsx
@@ -9,8 +9,7 @@ interface MenuButtonProps {
 
 const MenuButton: FC<MenuButtonProps> = ({icon, text, link}) => {
     return <Link href={link}>
-        <a className={"flex items-center hover:bg-gray-100 p-2 mx-2 rounded"}
-        >
+        <a className={"flex items-center w-full hover:bg-gray-100 p-2 mx-2 rounded"} >
             {icon} <span className={"ml-1.5"}>{text}</span> </a>
     </Link>
 }
--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,23 +1,14 @@
 import React, {FC} from "react";
 import {HiOutlineCollection, HiOutlineUser} from "react-icons/hi";
 import MenuButton from "./ButtonMenu";
+import cn from "classnames"
 
 interface MenuProps {
 
 }
 
 const Menu: FC<MenuProps> = ({}) => {
-    return <div className={"flex justify-evenly justify-center items-center " +
-        "hidden justify-between items-center w-full md:flex md:w-auto md:order-1"}>
-        {
-            [
-                {icon: <HiOutlineUser/>, name: "Резюме", link: "/"},
-                {icon: <HiOutlineCollection/>, name: "Портфолио", link: "/portfolio"},
-            ].map((el, i) => {
-                return <MenuButton key={i} icon={el.icon} text={el.name} link={el.link}/>
-            })
-        }
-    </div>
+    return <></>
 }
 
 export default Menu

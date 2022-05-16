40446bc2b43d2aa0353489cbd57113923d71db93
Alexander <sklevzhic@gmail.com>
2022-05-16 22:16:13 +0300

    fixed layout header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,63 +1,129 @@
-import React, {FC, useState} from "react";
-
-import Menu from "./Menu";
-import SearchInput from "./SearhInput";
-import {CgDarkMode} from 'react-icons/cg';
-import Link from "next/link";
-import cn from "classnames";
+import {Fragment} from 'react'
+import {Popover, Transition} from '@headlessui/react'
+import AvatarKlevzhits from "../assets/projects/klevzhits.jpg"
+import {GiHamburgerMenu} from "react-icons/gi"
 import {HiOutlineCollection, HiOutlineUser} from "react-icons/hi";
-import MenuButton from "./ButtonMenu";
+
+import {CgClose} from "react-icons/cg"
+import React, {FC} from "react";
+import Link from "next/link";
+
+
+const solutions = [
+    {
+        name: 'Резюме',
+        href: '/',
+        icon: HiOutlineUser
+    },
+    {
+        name: 'Портфолио',
+        href: '/portfolio',
+        icon: HiOutlineCollection
+    },
+]
 
 const Header: FC = () => {
-    let [isVisibleMenu, setIsVisibleMenu] = useState<boolean>(true)
-    return <header className={cn("sticky top-0 z-50 shadow justify-start flex flex-row bg-white md:flex-row md:justify-between", { "flex-col": isVisibleMenu } )}>
-        <div className={"flex flex-row hover:bg-gray-50 p-2 cursor-pointer"}>
-            <figcaption className="flex justify-center mr-3 space-x-4 text-left">
-                <img src="http://ipkip.bspu.by/admin-panel/vendor/kcfinder/upload/images/sovet/klevzhits.jpg"
-                     alt=""
-                     className="w-12 h-12 rounded-full" loading="lazy"/>
-            </figcaption>
-            <div className={"flex flex-col"}>
-                <Link href="/" className="text-xl font-bold text-gray-700">Alexander Klevzhits</Link>
-                <p className="text-xs text-gray-700">sklevzhic@gmail.com (copy)</p>
+    return <header className={"sticky top-0 z-50 bg-white"}>
+        <Popover className="relative bg-white">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                <div
+                    className="flex justify-between items-center border-b-2 border-gray-100 py-3 md:justify-start md:space-x-10">
+                    <div className="flex justify-start lg:w-0 lg:flex-1">
+                        <Link href="/">
+                            <img
+                                className="h-12 w-aut rounded-full cursor-pointer"
+                                src={AvatarKlevzhits.src}
+                                alt=""
+                            />
+                        </Link>
+                    </div>
+                    <div className="-mr-2 -my-2 md:hidden">
+                        <Popover.Button
+                            className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Open menu</span>
+                            <GiHamburgerMenu/>
+                            {/*<MenuIcon className="h-6 w-6" aria-hidden="true" />*/}
+                        </Popover.Button>
+                    </div>
 
+                    <Popover.Group as="nav" className="hidden md:flex space-x-10">
+                        {solutions.map((item) => (
+                            <Link href={item.href} key={item.name}>
+                                <a className={"flex"}>
+                                    <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                                               aria-hidden="true"/>
+                                    <span
+                                        className="text-base font-medium text-gray-500 ml-1 hover:text-gray-900 cursor-pointer">
+                                    {item.name}
+                                </span>
+                                </a>
+                            </Link>
+                        ))}
+
+
+                    </Popover.Group>
+
+
+                </div>
             </div>
 
-        </div>
-        <button type="button"
-                onClick={() => setIsVisibleMenu(!isVisibleMenu)}
-                 className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100
-                 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400
-                 dark:hover:bg-gray-700 dark:focus:ring-gray-600
-                 absolute top-3 right-3"
-        >
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                ></path>
-            </svg>
-            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                 xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                ></path>
-            </svg>
-        </button>
-        <div className={cn("flex justify-center items-center " +
-            " w-full md:flex md:flex-row  md:w-auto", {"hidden flex-row": !isVisibleMenu}, {"flex-col": isVisibleMenu})}>
-            {
-                [
-                    {icon: <HiOutlineUser/>, name: "Резюме", link: "/"},
-                    {icon: <HiOutlineCollection/>, name: "Портфолио", link: "/portfolio"},
-                ].map((el, i) => {
-                    return <MenuButton key={i} icon={el.icon} text={el.name} link={el.link}/>
-                })
-            }
-        </div>
 
-
+            <Transition
+                as={Fragment}
+                enter="duration-200 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <Popover.Panel focus
+                               className="z-10 absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                    <div
+                        className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                        <div className="pt-5 pb-6 px-5">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <img
+                                        className="h-10 w-auto sm:h-10 rounded-full"
+                                        src={AvatarKlevzhits.src}
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="-mr-2">
+                                    <Popover.Button
+                                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">Close menu</span>
+                                        <CgClose className="h-6 w-6" aria-hidden="true"/>
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <nav className="grid gap-y-8">
+                                    {solutions.map((item) => (
+                                        <Link
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 "
+                                        >
+                                            <a className={"flex"}>
+                                                <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                                                           aria-hidden="true"/>
+                                                <span
+                                                    className="ml-3 text-base font-medium text-gray-900 ml-1 cursor-pointer">{item.name}</span>
+                                            </a>
+                                        </Link>
+                                    ))}
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </Popover.Panel>
+            </Transition>
+        </Popover>
     </header>
 }
 
 export default Header
+
+
---
+++ app/components/Headercopy.tsx
@@ -0,0 +1,63 @@
+import React, {FC, useState} from "react";
+
+import Menu from "./Menu";
+import SearchInput from "./SearhInput";
+import {CgDarkMode} from 'react-icons/cg';
+import Link from "next/link";
+import cn from "classnames";
+import {HiOutlineCollection, HiOutlineUser} from "react-icons/hi";
+import MenuButton from "./ButtonMenu";
+
+const Header: FC = () => {
+    let [isVisibleMenu, setIsVisibleMenu] = useState<boolean>(true)
+    return <header className={cn("sticky top-0 z-50 shadow justify-start flex flex-row bg-white md:flex-row md:justify-between", { "flex-col": isVisibleMenu } )}>
+        <div className={"flex flex-row hover:bg-gray-50 p-2 cursor-pointer"}>
+            <figcaption className="flex justify-center mr-3 space-x-4 text-left">
+                <img src="http://ipkip.bspu.by/admin-panel/vendor/kcfinder/upload/images/sovet/klevzhits.jpg"
+                     alt=""
+                     className="w-12 h-12 rounded-full" loading="lazy"/>
+            </figcaption>
+            <div className={"flex flex-col"}>
+                <Link href="/" className="text-xl font-bold text-gray-700">Alexander Klevzhits</Link>
+                <p className="text-xs text-gray-700">sklevzhic@gmail.com (copy)</p>
+
+            </div>
+
+        </div>
+        <button type="button"
+                onClick={() => setIsVisibleMenu(!isVisibleMenu)}
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100
+                 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400
+                 dark:hover:bg-gray-700 dark:focus:ring-gray-600
+                 absolute top-3 right-3"
+        >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                ></path>
+            </svg>
+            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                ></path>
+            </svg>
+        </button>
+        <div className={cn("flex justify-center items-center " +
+            " w-full md:flex md:flex-row  md:w-auto", {"hidden flex-row": !isVisibleMenu}, {"flex-col": isVisibleMenu})}>
+            {
+                [
+                    {icon: <HiOutlineUser/>, name: "Резюме", link: "/"},
+                    {icon: <HiOutlineCollection/>, name: "Портфолио", link: "/portfolio"},
+                ].map((el, i) => {
+                    return <MenuButton key={i} icon={el.icon} text={el.name} link={el.link}/>
+                })
+            }
+        </div>
+
+
+    </header>
+}
+
+export default Header

ec2a840caa8f147c7c37cd347b1b69a960e3874b
sklevzhic <sklevzhic@gmail.com>
2022-06-01 11:42:29 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,24 @@ import Layout from "../app/layout/MainLayout";
 import React from "react";
 import { store } from "../store/index"
 import { Provider } from "react-redux";
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
     <div className={"bg-gray-100"}>
+      <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-DZB5CFBQW4"
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-DZB5CFBQW4');
+        `}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
